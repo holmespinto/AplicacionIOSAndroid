62485d80156e6ab7c6d7e2b1bfe0d3f350f3173f
holmes <holmespinto@unicesar.edu.co>
2023-05-19 09:19:45 -0500

    Finalizada pantalla contador

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { CounterScreen } from './src/components/CounterScreen/CounterScreen';
 
 export const App = () => {
-  return (
-    <View>
-      <Text>Hola mundo</Text>
-    </View>
-  );
+  return (<>
+    <CounterScreen/>
+    </> );
 };
